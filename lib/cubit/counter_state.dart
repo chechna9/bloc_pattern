4ad27aa55c3dc6aad1fa3df580af85fb2e889024
chechna9 <ja_bouchouareb@esi.dz>
2023-02-17 11:31:51 +0100

    bloc tests

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,7 +1,11 @@
 part of 'counter_cubit.dart';
 
-class CounterState {
+class CounterState extends Equatable {
   int counterValue;
   bool wasIncremented;
   CounterState({required this.counterValue, required this.wasIncremented});
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [counterValue, wasIncremented];
 }
---
+++ test/cubit/counter_cubit_test.dart
@@ -0,0 +1,33 @@
+import 'package:bloc_pattern_course/cubit/counter_cubit.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('counter cubit', () {
+    CounterCubit? counterCubit;
+    setUp(() {
+      counterCubit = CounterCubit();
+    });
+    tearDown(() {
+      counterCubit!.close();
+    });
+    test('the initial state of CounterCubit is CounterState(initialvalue:0)',
+        () {
+      expect(counterCubit!.state,
+          CounterState(counterValue: 0, wasIncremented: false));
+    });
+    // test from bloc_test
+    blocTest<CounterCubit, CounterState>(
+      'the cubit should emit a counterState(counterValue:1,wasIncremented:true) when cubit.increment() is called',
+      build: () => counterCubit!,
+      act: (cubit) => cubit.increment(),
+      expect: () => [CounterState(counterValue: 1, wasIncremented: true)],
+    );
+    blocTest<CounterCubit, CounterState>(
+      'the cubit should emit a counterState(counterValue:-1,wasIncremented:false) when cubit.decrementd() is called',
+      build: () => counterCubit!,
+      act: (cubit) => cubit.decrement(),
+      expect: () => [CounterState(counterValue: -1, wasIncremented: false)],
+    );
+  });
+}
